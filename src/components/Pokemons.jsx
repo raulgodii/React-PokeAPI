2e340914dfa0a3278b40e2df76cc0ef2b5041ae1
raulgodii <raulgodii13@gmail.com>
2024-02-08 17:53:17 +0100

    cargar mas

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,52 +1,79 @@
+import { useEffect, useState } from "react";
+
+let url = "";
 
 function Pokemons() {
+    const [pokemons, setPokemons] = useState([]);
+
+
+    function cargarMas() {
+        fetch(url)
+            .then((response) => response.json())
+            .then((datosPokemons) => {
+                console.log(datosPokemons)
+                setPokemons([...pokemons, ...datosPokemons.results])
+                url = datosPokemons.next;
+            });
+    }
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/?offset=2&limit=2")
+            .then((response) => response.json())
+            .then((datosPokemons) => {
+                console.log(datosPokemons)
+                url = datosPokemons.next;
+                setPokemons([...pokemons, ...datosPokemons.results])
+            });
+    }, []);
+
+    let pokemonsMaquetados = pokemons.map(pokemon =>
+
+        <div key={pokemon.name} className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
+            <div className="item">
+                <div className="thumb">
+                    <a href="product-details.html"><img src="assets/images/trending-01.jpg" alt="" /></a>
+                    <span className="price"><em>$36</em>$24</span>
+                </div>
+                <div className="down-content">
+                    <span className="category">Action</span>
+                    <h4>{pokemon.name}</h4>
+                    <a href="product-details.html"><i className="fa fa-shopping-bag"></i></a>
+                </div>
+            </div>
+        </div>
+    )
 
 
     return (
         <>
-            <div class="page-heading header-text">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
+            <div className="page-heading header-text">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
                             <h3>Pokemons</h3>
-                            <span class="breadcrumb"><a href="#">Home</a> &gt; Our Shop</span>
+                            <span className="breadcrumb"><a href="#">Home</a> &gt; Our Shop</span>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="section trending">
-                <div class="container">
-                    <ul class="trending-filter">
-                        <div class="search">
+            <div className="section trending">
+                <div className="container">
+                    <ul className="trending-filter">
+                        <div className="search">
                             <input placeholder="Search pokemons..." type="text" />
                             <button type="submit">Search</button>
                         </div>
                     </ul>
-                    <div class="row trending-box">
-                        <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
-                            <div class="item">
-                                <div class="thumb">
-                                    <a href="product-details.html"><img src="assets/images/trending-01.jpg" alt="" /></a>
-                                    <span class="price"><em>$36</em>$24</span>
-                                </div>
-                                <div class="down-content">
-                                    <span class="category">Action</span>
-                                    <h4>Assasin Creed</h4>
-                                    <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div className="row trending-box">
+                        {pokemonsMaquetados}
                     </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <ul class="pagination">
-                                <li><a href="#"> &lt; </a></li>
-                                <li><a href="#">1</a></li>
-                                <li><a class="is_active" href="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#"> &gt; </a></li>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <ul class="trending-filter">
+                                <li>
+                                    <a onClick={cargarMas} class="pointer">Show more</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
